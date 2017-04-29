fe04c3e063352498147961d3047f775e759e466f
Marek Lewandowski <marek@mlewandowski.com>
2017-04-30 00:55:22 +0200

    Added missing tc.

--- test/html.js
+++ test/html.js
@@ -15,6 +15,14 @@ describe( 'html helper', function() {
 			expect( ret ).to.be.eql( expectedFixture );
 		} );
 
+
+		it( 'Does not need sourceUrl', () => {
+			const expectedFixture = 'Version:0.9\r\nStartHTML:00000097\r\nEndHTML:00000268\r\nStartFragment:00000131\r\nEndFragment:00000232\r\n<html><body>\r\n<!--StartFragment-->The TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span><!--EndFragment-->\r\n</body>\r\n</html>';
+			const inputFixture = '<html><body>\r\n<!--StartFragment-->The TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span><!--EndFragment-->\r\n</body>\r\n</html>';
+
+			expect( html.encode( inputFixture ) ).to.be.eql( expectedFixture );
+		} );
+
 		it( 'Automatically inserts markers', () => {
 			const expectedFixture = 'Version:0.9\r\nStartHTML:00000128\r\nEndHTML:00000299\r\nStartFragment:00000160\r\nEndFragment:00000265\r\nSourceURL:https://www.w3.org/\r\n<html><body><!--StartFragment-->\r\nThe TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span>\r\n<!--EndFragment--></body>\r\n</html>';
 			const inputFixture = '<html><body>\r\nThe TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span>\r\n</body>\r\n</html>';
