8b82657ac6d91d8564ccdd7406a1fde4c3ae399c
Marek Lewandowski <marek@mlewandowski.com>
2017-04-30 00:52:53 +0200

    Basic implementation for html.decode.

--- test/html.js
+++ test/html.js
@@ -33,4 +33,18 @@ describe( 'html helper', function() {
 			expect( ret ).to.be.eql( expectedFixture );
 		} );
 	} );
+
+	describe( 'decode', () => {
+		it( 'works parses content with markers', () => {
+			const inputFixture = 'Version:0.9\r\nStartHTML:00000128\r\nEndHTML:00000299\r\nStartFragment:00000160\r\nEndFragment:00000265\r\nSourceURL:https://www.w3.org/\r\n<html><body><!--StartFragment-->\r\nThe TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span>\r\n<!--EndFragment--></body>\r\n</html>';
+
+			let ret = html.decode( inputFixture );
+
+			expect( ret ).to.be.eql( '\r\nThe TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span>\r\n' );
+		} );
+
+		it( 'returns correct val for empty input', () => {
+			expect( html.decode( '' ) ).to.be.eql( null );
+		} );
+	} );
 } );
