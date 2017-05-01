bf7c947faecee4d7e11b2fc09270ff12f5c52a8d
Marek Lewandowski <marek@mlewandowski.com>
2017-05-01 11:33:24 +0200

    Cleanup to make linter happy.

--- test/html.js
+++ test/html.js
@@ -5,10 +5,15 @@ const chai = require( 'chai' );
 const expect = chai.expect;
 
 describe( 'html helper', function() {
+	const expectedFixture = 'Version:0.9\r\nStartHTML:00000128\r\nEndHTML:00000299\r\nStartFragment:00000162\r\n' +
+		'EndFragment:00000263\r\nSourceURL:https://www.w3.org/\r\n<html><body>\r\n<!--StartFragment-->' +
+		'The TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span>' +
+		'<!--EndFragment-->\r\n</body>\r\n</html>';
+
 	describe( 'encode', () => {
 		it( 'Plays well with \r\n eols', () => {
-			const expectedFixture = 'Version:0.9\r\nStartHTML:00000128\r\nEndHTML:00000299\r\nStartFragment:00000162\r\nEndFragment:00000263\r\nSourceURL:https://www.w3.org/\r\n<html><body>\r\n<!--StartFragment-->The TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span><!--EndFragment-->\r\n</body>\r\n</html>';
-			const inputFixture = '<html><body>\r\n<!--StartFragment-->The TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span><!--EndFragment-->\r\n</body>\r\n</html>';
+			const inputFixture = '<html><body>\r\n<!--StartFragment-->The TurinSir Timâ€™s <span class="dtstart published" ' +
+				'title="2017-04-18T17:50:34Z">8 April 2017</span><!--EndFragment-->\r\n</body>\r\n</html>';
 
 			let ret = html.encode( inputFixture, 'https://www.w3.org/' );
 
@@ -17,15 +22,21 @@ describe( 'html helper', function() {
 
 
 		it( 'Does not need sourceUrl', () => {
-			const expectedFixture = 'Version:0.9\r\nStartHTML:00000097\r\nEndHTML:00000268\r\nStartFragment:00000131\r\nEndFragment:00000232\r\n<html><body>\r\n<!--StartFragment-->The TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span><!--EndFragment-->\r\n</body>\r\n</html>';
-			const inputFixture = '<html><body>\r\n<!--StartFragment-->The TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span><!--EndFragment-->\r\n</body>\r\n</html>';
+			const expectedFixture = 'Version:0.9\r\nStartHTML:00000097\r\nEndHTML:00000268\r\nStartFragment:00000131\r\n' +
+				'EndFragment:00000232\r\n<html><body>\r\n<!--StartFragment-->The TurinSir Timâ€™s <span class="dtstart published" ' +
+				'title="2017-04-18T17:50:34Z">8 April 2017</span><!--EndFragment-->\r\n</body>\r\n</html>';
+			const inputFixture = '<html><body>\r\n<!--StartFragment-->The TurinSir Timâ€™s <span class="dtstart published" ' +
+				'title="2017-04-18T17:50:34Z">8 April 2017</span><!--EndFragment-->\r\n</body>\r\n</html>';
 
 			expect( html.encode( inputFixture ) ).to.be.eql( expectedFixture );
 		} );
 
 		it( 'Automatically inserts markers', () => {
-			const expectedFixture = 'Version:0.9\r\nStartHTML:00000128\r\nEndHTML:00000299\r\nStartFragment:00000160\r\nEndFragment:00000265\r\nSourceURL:https://www.w3.org/\r\n<html><body><!--StartFragment-->\r\nThe TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span>\r\n<!--EndFragment--></body>\r\n</html>';
-			const inputFixture = '<html><body>\r\nThe TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span>\r\n</body>\r\n</html>';
+			const expectedFixture = 'Version:0.9\r\nStartHTML:00000128\r\nEndHTML:00000299\r\nStartFragment:00000160\r\n' +
+				'EndFragment:00000265\r\nSourceURL:https://www.w3.org/\r\n<html><body><!--StartFragment-->\r\nThe TurinSir Timâ€™s' +
+				' <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span>\r\n<!--EndFragment--></body>\r\n</html>';
+			const inputFixture = '<html><body>\r\nThe TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April ' +
+				'2017</span>\r\n</body>\r\n</html>';
 
 			let ret = html.encode( inputFixture, 'https://www.w3.org/' );
 
@@ -33,7 +44,8 @@ describe( 'html helper', function() {
 		} );
 
 		it( 'Generates markers also for hopeless HTML', () => {
-			const expectedFixture = 'Version:0.9\nStartHTML:00000122\nEndHTML:00000177\nStartFragment:00000142\nEndFragment:00000159\nSourceURL:https://www.w3.org/\n<!--StartFragment-->foo<b>bar\nbaz</b><!--EndFragment-->';
+			const expectedFixture = 'Version:0.9\nStartHTML:00000122\nEndHTML:00000177\nStartFragment:00000142\n' +
+				'EndFragment:00000159\nSourceURL:https://www.w3.org/\n<!--StartFragment-->foo<b>bar\nbaz</b><!--EndFragment-->';
 			const inputFixture = 'foo<b>bar\nbaz</b>';
 
 			let ret = html.encode( inputFixture, 'https://www.w3.org/' );
@@ -43,7 +55,9 @@ describe( 'html helper', function() {
 	} );
 
 	describe( 'decode', () => {
-		const inputFixture = 'Version:0.9\r\nStartHTML:00000128\r\nEndHTML:00000299\r\nStartFragment:00000160\r\nEndFragment:00000265\r\nSourceURL:https://www.w3.org/\r\n<html><body><!--StartFragment-->\r\nThe TurinSir Timâ€™s <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span>\r\n<!--EndFragment--></body>\r\n</html>';
+		const inputFixture = 'Version:0.9\r\nStartHTML:00000128\r\nEndHTML:00000299\r\nStartFragment:00000160\r\n' +
+			'EndFragment:00000265\r\nSourceURL:https://www.w3.org/\r\n<html><body><!--StartFragment-->\r\nThe TurinSir Timâ€™s' +
+			' <span class="dtstart published" title="2017-04-18T17:50:34Z">8 April 2017</span>\r\n<!--EndFragment--></body>\r\n</html>';
 
 		it( 'Works parses content with markers', () => {
 			let ret = html.decode( inputFixture );
